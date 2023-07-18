3e47f2f173ba2209dded5d59ccfbee883361e3fa
Afraim <70313535+Shikamaru77@users.noreply.github.com>
2023-07-18 23:39:35 +0600

    pp

--- script.js
+++ script.js
@@ -43,3 +43,18 @@ for (var i = 0; i < navLinks.length; i++) {
     menuBtn.style.pointerEvents = "auto";
   });
 }
+//Skillset
+var v1 = $(".i1").attr("data-pg");
+$(".i1").animate({
+  width: v1
+}, 2000);
+var v2 = $(".i2").attr("data-pg");
+$(".i2").animate({
+  width: v2
+}, 2000);
+var v3 = $(".i3").attr("data-pg");
+$(".i3").animate({
+  width: v3
+}, 2000);
+/*var v4 = $(".i4").attr("data-pg");
+$(".i4").animate({width:v4},2000);*/
